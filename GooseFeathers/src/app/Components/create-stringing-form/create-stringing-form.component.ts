2651392183ba93d6dcb67deffa27dedad7265f56
Epicnessism <tony.jy.wang@gmail.com>
2024-09-14 11:20:59 -0400

    mat select at least works now

--- GooseFeathers/src/app/Components/create-stringing-form/create-stringing-form.component.ts
+++ GooseFeathers/src/app/Components/create-stringing-form/create-stringing-form.component.ts
@@ -7,6 +7,9 @@ import {MatButtonModule} from '@angular/material/button';
 import {StringingService} from "../../Services/stringing.service";
 import {CreateStringRequest} from "../../interfaces/CreateStringRequest";
 import {Racket} from "../../interfaces/Racket";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-create-stringing-form',
@@ -18,6 +21,9 @@ import {Racket} from "../../interfaces/Racket";
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatOption,
+    MatSelect,
+    NgForOf,
   ],
   templateUrl: './create-stringing-form.component.html',
   styleUrl: './create-stringing-form.component.scss'
@@ -33,13 +39,13 @@ export class CreateStringingFormComponent implements OnInit {
       console.log(data)
       this.racketModels = data
     })
+    this.firstFormGroup.addControl("racketId", this.racketId)
   }
 
-  public firstFormGroup: FormGroup<CreateStringingRequestFormControlOne> = new FormGroup<CreateStringingRequestFormControlOne>({
-    make: new FormControl('', {validators: Validators.required, nonNullable: true}),
-    model: new FormControl('', {validators: Validators.required, nonNullable: true})
+  racketId: FormControl = new FormControl('', {validators: Validators.required, nonNullable: true})
+  public firstFormGroup: FormGroup = new FormGroup({
+    // racketId: new FormControl('', {validators: Validators.required, nonNullable: true})
   })
-
   public secondFormGroup: FormGroup<CreateStringingRequestFormControlTwo> = new FormGroup<CreateStringingRequestFormControlTwo>({
     mains: new FormControl(0, {validators: Validators.required, nonNullable: true}),
     crosses: new FormControl(0, {validators: Validators.required, nonNullable: true}),
@@ -50,6 +56,9 @@ export class CreateStringingFormComponent implements OnInit {
 
   createStringRequest(): void {
     console.log("Create String Request")
+    let racketModel: Racket = this.racketModels.find(racket => racket.id == this.firstFormGroup.value.racketId)!;
+    console.log("Racket Model found as: " + racketModel)
+
     let request: CreateStringRequest = {
       crosses: this.secondFormGroup.value.crosses ?? -1,
       crossesInMeters: 4.5,
@@ -60,8 +69,8 @@ export class CreateStringingFormComponent implements OnInit {
       price: -1,
       racketToString: {
         racketId: "2783e281-873a-4e72-8973-fe60663a822e", //todo why is null/undefined not okay but okay in postman
-        make: this.firstFormGroup.value.make ?? '',
-        model: this.firstFormGroup.value.model ?? '',
+        make: racketModel.make ?? '',
+        model: racketModel.model ?? '',
         ownerDetails: {
           userId: "92cba104-c922-4852-a683-b32ffd21b109" //todo make this session user
         }
@@ -74,8 +83,7 @@ export class CreateStringingFormComponent implements OnInit {
 }
 
 interface CreateStringingRequestFormControlOne {
-  make: FormControl<string>;
-  model: FormControl<string>;
+  racketId: FormControl<string>;
 
 }
 
